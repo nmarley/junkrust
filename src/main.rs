ec96323d172587e9649f4a6022f12820088bd3e7
Nathan Marley <nathan.marley@gmail.com>
2021-01-09 09:56:58 -0300

    Fix bit index (Rust does not allow overflow shift)

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,23 @@ fn main() {
     // println!("n = {}", n);
 
     let mut bv: [u8; 8192] = [0; 8192];
-    println!("bv[0] = {}", bv[0]);
+    println!("bv[0] = {:08b}", bv[0]);
 
     let randos: Vec<u16> = vec![
         38405, 33022, 560, 59256, 60596, 32679, 9642, 31328, 64408, 55685,
     ];
 
     load_bit_vector(&mut bv, &randos);
-    println!("bv[4800] = {}", bv[4800]);
+    println!("bv[4800] = {:08b}", bv[4800]);
 }
 
 fn load_bit_vector(bv: &mut [u8; 8192], randos: &[u16]) {
     for rando in randos {
-        let byte_index = (rando / 8);
-        let bit_index = 1 << ((rando % 8) - 1);
+        let byte_index = rando / 8;
+        let bit_index = match rando % 8 {
+            0 => 0,
+            modulus => 1 << (modulus - 1),
+        } as u8;
         bv[byte_index as usize] |= bit_index;
     }
 }
