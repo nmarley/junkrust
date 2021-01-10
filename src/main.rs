1318483bf5900a81f8351eedd45cea9f50a7c15d
Nathan Marley <nathan.marley@gmail.com>
2021-01-09 22:17:51 -0300

    Use vec for heap allocation to avoid blowing stack

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
 use rand::Rng;
 
 fn main() {
-    // let n = ((u8::MAX - 7) / 8) + 8;
-    // println!("n = {}", n);
-    // 8192
-    let _n = (u16::MAX as u32 + 1) / 8;
-    // println!("n = {}", n);
-
-    let mut bv: [u8; 8192] = [0; 8192];
+    // let mut bv: Vec<u8> = Vec::with_capacity((((u32::MAX - 7) / 8) + 1) as usize);
+    let mut bv: Vec<u8> = vec![0; (((u32::MAX - 7) / 8) + 1) as usize];
     println!("bv[0] = {:08b}", bv[0]);
 
-    let randos: Vec<u16> = vec![
+    let randos: Vec<u32> = vec![
         38405, 33022, 560, 59256, 60596, 32679, 9642, 31328, 64408, 55685,
     ];
 
@@ -18,7 +13,7 @@ fn main() {
     println!("bv[4800] = {:08b}", bv[4800]);
 }
 
-fn load_bit_vector(bv: &mut [u8; 8192], randos: &[u16]) {
+fn load_bit_vector(bv: &mut [u8], randos: &[u32]) {
     for rando in randos {
         let byte_index = rando / 8;
         let bit_index = match rando % 8 {
@@ -33,6 +28,6 @@ fn load_bit_vector(bv: &mut [u8; 8192], randos: &[u16]) {
 fn gen_randos() {
     let mut rng = rand::thread_rng();
     for _i in 0..10 {
-        println!("{}", rng.gen_range(0..u16::MAX));
+        println!("{}", rng.gen_range(0..u32::MAX));
     }
 }
